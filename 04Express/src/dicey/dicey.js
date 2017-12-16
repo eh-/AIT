31f07816ed14a1131ece7d9cf9f6507aabc152a4
eh- <ehuang0000@bths.edu>
2017-12-15 22:13:05 -0500

    setup

---
+++ 04Express/src/dicey/dicey.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+	res.
+});
+app.get('/dice', function(req, res){
+	res.redirect('/');
+});
+app.get('/about', function(req, res){
+	
+});
+
+app.listen(8080);
