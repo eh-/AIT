77100f78ee8eb5056c6a03157b9740bd93f9cf19
eh- <ehuang0000@bths.edu>
2017-12-27 21:07:51 -0500

    add comments to links

--- 05LinkAggre/ait-news/app.js
+++ 05LinkAggre/ait-news/app.js
@@ -21,25 +21,52 @@ const Link = mongoose.model('Link');
 app.get('/', function(req, res){
 	Link.find(function(err, foundLinks, count){
 		if(foundLinks.length == 0){
-			res.render('index');
+			res.render('index', {'title': '480 News!'});
 		}
 		else{
-			res.render('index', {'subLinks': foundLinks});
+			res.render('index', {'title': '480 News!', 'subLinks': foundLinks});
 		}
 	});
 });
-app.post('/', function(req, res){
+app.get('/:currSlug', function(req, res){
+	Link.findOne({'slug': req.params.currSlug}, function(err, foundLink){
+		if(foundLink){
+			res.render('linkComs', {
+				'title': foundLink.title, 
+				'currTitle': foundLink.title,
+				'currUrl': foundLink.url,
+				'currComs': foundLink.comments,
+				'currSlug': foundLink.slug,
+			});
+		}
+		else{
+			res.render('error', {'title': '480 News!', 'errorMessage': err});
+		}
+	});
+});
+app.post('/subLink', function(req, res){
 	const addedLink = new Link({
 		url: req.body.suburl,
 		title: req.body.subtitle,
 	});
 	addedLink.save(function(err, addedLink, count){
 		if(err){
-			res.render('error', {'errorMessage': err})
+			res.render('error', {'title': '480 News!', 'errorMessage': err});
 		}
 		else{
 			console.log('added ', addedLink, count, err);	
 			res.redirect('/');
+		}
+	});
+});
+app.post('/subCom', function(req, res){
+	console.log(req.body);
+	Link.findOneAndUpdate({'slug': req.body.slug},{$push: {comments: {text: req.body.newCom, user: req.body.newName}}}, function(err, foundLink){
+		if(err){
+			res.render('error', {'title': '480 News!', 'errorMessage': err});
+		}
+		else{
+			res.redirect(foundLink.slug);
 		}
 	});
 });
