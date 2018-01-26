073fe0b2af18ee4823ece41e186aa963809cb1b2
eh- <ehuang0000@bths.edu>
2018-01-26 18:09:16 -0500

    boilerplate setup

---
+++ labSocketio/app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.use(express.static('public'));
+
+server.listen(3000);
