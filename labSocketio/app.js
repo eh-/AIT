c643d5440bca7ef84d8a8f52a3e2854b7ab90d28
eh- <ehuang0000@bths.edu>
2018-01-27 18:26:35 -0500

    working buttons, winner message, new connections

--- labSocketio/app.js
+++ labSocketio/app.js
@@ -4,4 +4,25 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 app.use(express.static('public'));
 
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+io.on('connection', function(socket){
+	socket.broadcast.emit('newcon', socket.id);
+	console.log(socket.id, ' connected');
+	socket.on('disconnect', function(){
+		console.log(socket.id, ' disconnected');
+	});
+	
+	socket.on('clicked', function(btn){
+		console.log('clicked: ', btn);
+		io.sockets.emit('clicked', btn);
+	});
+	
+	socket.on('coords', function(coords){
+		io.sockets.emit('coords', coords);
+	});
+});
+
 server.listen(3000);
--- labSocketio/public/racer.js
+++ labSocketio/public/racer.js
@@ -1,1 +1,46 @@
-const socket = io();
+function moveFace(face){
+	var player;
+	if(face === 'firstBtn')
+		player = document.querySelector('.player1');
+	else if(face === 'secBtn')
+		player = document.querySelector('.player2');
+	var newleft = parseInt(window.getComputedStyle(player).getPropertyValue('left'));
+	player.style.left = (++newleft).toString() + 'px';
+	if(newleft > 150){
+		document.querySelector('.player1Btn').classList.add('hidden');
+		document.querySelector('.player2Btn').classList.add('hidden');
+		document.querySelector('.winner').classList.remove('hidden');
+		document.querySelector('.winner').appendChild(document.createTextNode(face === 'firstBtn' ? 'Tears of Joy WINS' : 'Face Screaming WINS'));
+	}
+}
+
+function main(){
+	const socket = io();
+	socket.on('clicked', moveFace);
+	socket.on('newcon', function(socketid){
+		let first = parseInt(window.getComputedStyle(document.querySelector('.player1')).getPropertyValue('left'));
+		let sec = parseInt(window.getComputedStyle(document.querySelector('.player2')).getPropertyValue('left'));
+		socket.emit('coords', {'first': first, 'sec': sec});
+	});
+	socket.on('coords', function(coords){
+		console.log(coords);
+		document.querySelector('.player1').style.left = coords['first'].toString() + 'px';
+		document.querySelector('.player2').style.left = coords['sec'].toString() + 'px';
+	});
+	
+	let firstBtn = document.querySelector('.player1Btn');
+	firstBtn.addEventListener('click', function(evt){
+		console.log('clicked first');
+		evt.preventDefault();
+		socket.emit('clicked', 'firstBtn');
+	});
+
+	let secBtn = document.querySelector('.player2Btn');
+	secBtn.addEventListener('click', function(evt){
+		console.log('clicked second');
+		evt.preventDefault();
+		socket.emit('clicked', 'secBtn');
+	});
+}
+
+document.addEventListener('DOMContentLoaded', main);
