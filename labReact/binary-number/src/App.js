dbdaa00217b78d3ba72440d42e92682df0bc7b11
eh- <ehuang0000@bths.edu>
2018-02-20 21:48:17 -0500

    bitnumbers working

--- labReact/binary-number/src/App.js
+++ labReact/binary-number/src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var numBits = 8;
+
+class Bit extends Component{
+	render(){
+		return(<div className='bits' onClick={this.props.onClick}>{this.props.val}</div>);
+	}
+	
+}
+
 class App extends Component {
+	constructor(){
+		super();
+		var bitVals = [];
+		for(let i = 0; i < numBits; i++){
+			bitVals.push(0);
+		}
+		this.state = {
+			bitVals: bitVals,
+			total: 0
+		}
+	}
+	
   render() {
+		var divs = [];
+		for(let i = numBits - 1; i >= 0; i--){
+			divs.push(<Bit onClick={() => {this.handleClick(i)}} bitPosi={i} val={this.state.bitVals[i]} />);
+		}
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+			{divs}
+			<div className='bits'>=</div>
+			<div className='bits'>{this.state.total}</div>
       </div>
     );
   }
+	
+	handleClick(position){
+		var currBitVals = this.state.bitVals;
+		var currTotal = this.state.total;
+		if(this.state.bitVals[position] === 0){
+			currBitVals[position] = 1;
+			currTotal += Math.pow(2, position);
+		}
+		else if(this.state.bitVals[position] === 1){
+			currBitVals[position] = 0;
+			currTotal -= Math.pow(2, position);
+		}
+		this.setState({
+			bitVals: currBitVals,
+			total: currTotal,
+		});
+	}
 }
 
 export default App;
